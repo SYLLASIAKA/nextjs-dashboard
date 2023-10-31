02e5afcd171414d7774f09546bff7140a250a298
Sylla Siaka <Syllasiaka227@gmail.com>
2023-10-31 21:30:57 +0000

    adding search and pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,12 +1,7 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import Cards from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {
-  fetchLatestInvoices,
-  fetchCardData
-} from '@/app/lib/data';
 import { Suspense } from 'react';
 import { 
   RevenueChartSkeleton,
@@ -15,13 +10,6 @@ import {
 } from '@/app/ui/skeletons';
 
 export default async function Page() {
-  // const latestInvoices = await fetchLatestInvoices();
-  // const {
-  //   numberOfInvoices,
-  //   numberOfCustomers,
-  //   totalPaidInvoices,
-  //   totalPendingInvoices,
-  // } = await fetchCardData();
 
   return (
     <main>
@@ -29,14 +17,6 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
          <Suspense fallback={<CardsSkeleton />}>
           <Cards />
         </Suspense>
@@ -45,7 +25,6 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
         <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
         </Suspense>
