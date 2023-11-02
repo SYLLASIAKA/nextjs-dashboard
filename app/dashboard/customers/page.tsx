1bc04f7e1f0537974cf1f9f95fbd1d4fb1ceabe0
Sylla Siaka <Syllasiaka227@gmail.com>
2023-11-02 13:47:37 +0000

    Finish Project

--- app/dashboard/customers/page.tsx
+++
@@ -1,9 +0,0 @@
-import { fetchFilteredCustomers } from "@/app/lib/data";
-import CustomersTable from "@/app/ui/customers/table";
-
-export default function Page() {
-
-    return (
-        <p>Dashboard Customers</p>
-    )
-}
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -15,7 +15,6 @@ const links = [
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
